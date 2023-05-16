09040a6d780b139f868ac0153a9bff18fefaad4f
Darragh-Grealish <dgrealish2003@gmail.com>
2023-05-16 16:02:44 +0100

    Add Vowel check method

--- PigLatin.java
+++ PigLatin.java
@@ -1,5 +1,36 @@
+
 public class PigLatin {
     public static void main(String[] args) {
-        
+        String str = pigLatin("graphic");
+        System.out.print(str);
     }
+    
+     
+    static String pigLatin(String word) {
+
+        if (isVowel(word.charAt(0))) {
+        // If a word starts with a vowel add the word "way" at the end of the word.
+        // Example: Awesome = Awesome +way = Awesomeway
+        }
+
+        else if (isVowel(word.charAt(1))) {
+            // If a word starts with a consonant and a vowel, put the first letter of the word at the end of the word and add "ay."
+            // Example: Happy = appyh + ay = appyhay
+        }
+
+        else{
+        // If a word starts with two consonants move the two consonants to the end of the word and add "ay."
+        // Example: Child = Ildch + ay = Ildchay
+        }
+ 
+        return "ay";
+    }
+
+
+    // Check for vowel
+    static boolean isVowel(char c) {
+        return (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' ||c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+    }
+
+
 }
